c9dab3fc919076c3cebe6f4f9bb28ff0500972e0
drm121918-dev <drm121918@gmail.com>
2025-11-26 17:42:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,6 +2,8 @@
 # Ask Doubt on telegram @Tushar0125
 
 from os import environ
+👉 vars.py ফাইলের একদম উপরে এই লাইনটা যোগ করুন:
+
 
 API_ID = int(environ.get("API_ID", "36925285")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c") #Replace with your api hash

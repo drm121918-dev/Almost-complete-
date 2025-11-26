41f99d7cbdd027cbeadee024ba61fbf1d523fd99
drm121918-dev <drm121918@gmail.com>
2025-11-26 19:55:03 +0530

    Add PORT and DATABASE_URL to vars.py
    Added PORT and DATABASE_URL variables for configuration.

--- vars.py
+++ vars.py
@@ -1,13 +1,10 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
 from os import environ
-👉 vars.py ফাইলের একদম উপরে এই লাইনটা যোগ করুন:
 
+PORT = int(environ.get("PORT", 8870))
 
-API_ID = int(environ.get("API_ID", "36925285")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "36925285"))
+API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PORT = int(os.environ.get("PORT", 8870))  # Default to 8000 if not set
+DATABASE_URL = environ.get("mongodb+srv://drm121918_db_user:<db_KtwMBKSvKo56817L>@almostcomplete.vxtrirx.mongodb.net/?appName=Almostcomplete", "mongodb://localhost:27017/your_bot_db")
 

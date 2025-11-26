12e51c78d9923de06ec1ed0958bf762d62f27c98
drm121918-dev <drm121918@gmail.com>
2025-11-26 14:55:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22946135")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527") #Replace with your api hash
+API_ID = int(environ.get("API_ID", "36925285")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "ef3e2c581370c93287854dc36d78c13c") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
 
 # Database Configuration for MongoDB
